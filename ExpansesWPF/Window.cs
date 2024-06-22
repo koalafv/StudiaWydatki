7bb57d742b76f0804a8740bc6d1738ce5fbd6fd6
Lukasz Kramorz <lukasz.kramorz@aspekt.net.pl>
2024-06-22 14:08:18 +0200

    diagram

---
+++ ExpansesWPF/Window.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using MessageBox = System.Windows.MessageBox;
+
+namespace ExpansesWPF
+{
+	public class Window : System.Windows.Window
+	{
+		public void ErrorBox(string message) => MessageBox.Show(message, "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+
+		public void WarningBox(string message) => MessageBox.Show(message, "Uwaga", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+
+		public void SuccesBox(string message) => MessageBox.Show(message, "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
+	}
+}
